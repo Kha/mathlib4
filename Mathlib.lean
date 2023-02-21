83dc9f75646707913c49718018f5e0a9bdf9c87e
casavaca <96765450+casavaca@users.noreply.github.com>
2023-02-20 22:30:41 -0800

    automated fixes
    Mathbin -> Mathlib
    
    fix certain import statements
    
    move "by" to end of line
    
    add import to Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -609,6 +609,7 @@ import Mathlib.Data.Rat.Sqrt
 import Mathlib.Data.Real.Basic
 import Mathlib.Data.Real.CauSeq
 import Mathlib.Data.Real.CauSeqCompletion
+import Mathlib.Data.Real.ConjugateExponents
 import Mathlib.Data.Real.NNReal
 import Mathlib.Data.Real.Pointwise
 import Mathlib.Data.Real.Sign
