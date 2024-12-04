5a448360133c0dca306fb3d6e532c27e18302a46
sgouezel <sebastien.gouezel@univ-rennes1.fr>
2024-12-04 17:15:54 +0100

    progress

--- Mathlib/Analysis/NormedSpace/Multilinear/Curry.lean
+++ Mathlib/Analysis/NormedSpace/Multilinear/Curry.lean
@@ -554,6 +554,28 @@ section
 
 variable (𝕜 G G') {k l : ℕ} {s : Finset (Fin n)}
 
+#where
+
+def finSumEquivOfFinset {α β : Type*} {s : Set ι} (e : {x : ι | x ∈ s} ≃ α)
+    (e' : {x : ι | x ∉ s} ≃ β) [DecidablePred fun x ↦ x ∈ s]
+    [Fintype α] [Fintype β] : α ⊕ β ≃ ι :=
+  (Equiv.sumCongr e.symm e'.symm).trans (Equiv.Set.sumCompl _)
+
+
+/-- If `s : Finset (Fin n)` is a finite set of cardinality `k` and its complement has cardinality
+`l`, then the space of continuous multilinear maps `G [×n]→L[𝕜] G'` of `n` variables is isomorphic
+to the space of continuous multilinear maps `G [×k]→L[𝕜] G [×l]→L[𝕜] G'` of `k` variables taking
+values in the space of continuous multilinear maps of `l` variables. -/
+def coulis {α β : Type*} {s : Finset ι} (e : {x : ι | x ∈ s} ≃ α) (e' : {x : ι | x ∉ s} ≃ β)
+    [Fintype α] [Fintype β] [DecidableEq ι] :
+    ContinuousMultilinearMap 𝕜 (fun (_ : ι) ↦ G) G' ≃ₗᵢ[𝕜]
+    ContinuousMultilinearMap 𝕜 (fun (_ : α) ↦ G) (ContinuousMultilinearMap 𝕜 (fun (_ : β) ↦ G) G')
+    :=
+  (domDomCongrₗᵢ 𝕜 G G' ((Equiv.sumCongr e.symm e'.symm).trans (Equiv.Set.sumCompl _)).symm).trans
+    (currySumEquiv 𝕜 α β G G')
+
+#exit
+
 /-- If `s : Finset (Fin n)` is a finite set of cardinality `k` and its complement has cardinality
 `l`, then the space of continuous multilinear maps `G [×n]→L[𝕜] G'` of `n` variables is isomorphic
 to the space of continuous multilinear maps `G [×k]→L[𝕜] G [×l]→L[𝕜] G'` of `k` variables taking
